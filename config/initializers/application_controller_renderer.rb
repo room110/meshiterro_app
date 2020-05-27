d3cfc063a01dee9767b22cb1174845c0cd4f1b21
yukiito <ito.room.room.110@gmail.com>
2020-05-27 13:59:01 +0900

    [Finish]5-3

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '3ac337795274aded9412545482952097f1282a859b065759d651adf2bd90cb0907ff6e241d938b328cec355b450204fff4d0a4c792d56ae1c5fb2fe7064a5a37'
